29678f8832b2f4c83143a59b783ccea83671d90d
Lucas Tamir Faria <lucas.tamir@rarolabs.com.br>
2021-06-10 16:27:23 -0300

    refactor: clearing example

--- test/model/client.dart
+++ test/model/client.dart
@@ -3,8 +3,3 @@ class Client {
   String cpf;
   Client({required this.name, required this.cpf});
 }
-
-main() {
-  var client = Client(name: "João Grilo", cpf: "545.656.985-36");
-  print("${client.name} ${client.cpf}");
-}
